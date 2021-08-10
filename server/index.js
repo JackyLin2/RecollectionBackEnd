bfc8bafcb35930f20be8bdc98768df8329bcafc8
JackyLin2 <jacky.jia.lin@gmail.com>
2021-08-10 12:34:30 -0400

    commit on main

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors()); 
 
+app.get('/', (req,res) => {
+    res.send('Welcome to Recollection API')
+})
+
 app.use('/posts', postRoutes)
 const PORT = process.env.PORT || 5000; 
 
